53cc2a8f731269414c5263e7da3293dbc28b676c
Grady McDowell <gradyrmcd@gmail.com>
2015-01-04 10:45:58 -0500

    Auto increment ID
    Fixed bug where only 1 user could register with the form

--- users.schema.sql
+++ users.schema.sql
@@ -28,7 +28,7 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE IF NOT EXISTS `users` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(65) NOT NULL,
   `password` text NOT NULL,
   `email` text NOT NULL,
